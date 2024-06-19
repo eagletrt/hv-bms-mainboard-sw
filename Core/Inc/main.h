24bb66e9a954f1b69cbf1d0b2d02603c841b992e
gmazzucchi <giacomo.mazzucchi@protonmail.com>
2024-06-19 17:08:38 +0200

    init: pins setup from pcb schematic

---
+++ Core/Inc/main.h
@@ -0,0 +1,233 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SPARE_1_Pin                         GPIO_PIN_2
+#define SPARE_1_GPIO_Port                   GPIOE
+#define SPARE_2_Pin                         GPIO_PIN_3
+#define SPARE_2_GPIO_Port                   GPIOE
+#define SPARE_3_Pin                         GPIO_PIN_4
+#define SPARE_3_GPIO_Port                   GPIOE
+#define SPARE_4_Pin                         GPIO_PIN_5
+#define SPARE_4_GPIO_Port                   GPIOE
+#define SPARE_5_Pin                         GPIO_PIN_6
+#define SPARE_5_GPIO_Port                   GPIOE
+#define SPARE_6_Pin                         GPIO_PIN_13
+#define SPARE_6_GPIO_Port                   GPIOC
+#define SPARE_7_Pin                         GPIO_PIN_14
+#define SPARE_7_GPIO_Port                   GPIOC
+#define SPARE_8_Pin                         GPIO_PIN_15
+#define SPARE_8_GPIO_Port                   GPIOC
+#define SPARE_9_Pin                         GPIO_PIN_0
+#define SPARE_9_GPIO_Port                   GPIOF
+#define SPARE_10_Pin                        GPIO_PIN_1
+#define SPARE_10_GPIO_Port                  GPIOF
+#define SPARE_11_Pin                        GPIO_PIN_2
+#define SPARE_11_GPIO_Port                  GPIOF
+#define AIRN_OPEN_MEC_Pin                   GPIO_PIN_3
+#define AIRN_OPEN_MEC_GPIO_Port             GPIOF
+#define AIRP_OPEN_MEC_Pin                   GPIO_PIN_4
+#define AIRP_OPEN_MEC_GPIO_Port             GPIOF
+#define IMD_OK_Pin                          GPIO_PIN_5
+#define IMD_OK_GPIO_Port                    GPIOF
+#define PLAUSIBLE_STATE_RC_Pin              GPIO_PIN_6
+#define PLAUSIBLE_STATE_RC_GPIO_Port        GPIOF
+#define TSAL_GREEN_Pin                      GPIO_PIN_7
+#define TSAL_GREEN_GPIO_Port                GPIOF
+#define AMB_TEMP_Pin                        GPIO_PIN_9
+#define AMB_TEMP_GPIO_Port                  GPIOF
+#define PROBING_3V3_Pin                     GPIO_PIN_10
+#define PROBING_3V3_GPIO_Port               GPIOF
+#define COOLING_CONSUMPTION_Pin             GPIO_PIN_0
+#define COOLING_CONSUMPTION_GPIO_Port       GPIOC
+#define ITS_HC_F_Pin                        GPIO_PIN_1
+#define ITS_HC_F_GPIO_Port                  GPIOC
+#define ITS_LC_F_Pin                        GPIO_PIN_2
+#define ITS_LC_F_GPIO_Port                  GPIOC
+#define AUX_TEMP_Pin                        GPIO_PIN_3
+#define AUX_TEMP_GPIO_Port                  GPIOC
+#define SD_OUT_Pin                          GPIO_PIN_0
+#define SD_OUT_GPIO_Port                    GPIOA
+#define SD_IN_Pin                           GPIO_PIN_1
+#define SD_IN_GPIO_Port                     GPIOA
+#define SD_END_Pin                          GPIO_PIN_2
+#define SD_END_GPIO_Port                    GPIOA
+#define V5_MCU_Pin                          GPIO_PIN_3
+#define V5_MCU_GPIO_Port                    GPIOA
+#define DAC_PUMP_Pin                        GPIO_PIN_4
+#define DAC_PUMP_GPIO_Port                  GPIOA
+#define INLET_LIQUID_TEMP_Pin               GPIO_PIN_5
+#define INLET_LIQUID_TEMP_GPIO_Port         GPIOA
+#define OUTLET_LIQUID_TEMP_1_Pin            GPIO_PIN_6
+#define OUTLET_LIQUID_TEMP_1_GPIO_Port      GPIOA
+#define OUTLET_LIQUID_TEMP_2_Pin            GPIO_PIN_7
+#define OUTLET_LIQUID_TEMP_2_GPIO_Port      GPIOA
+#define OUTLET_LIQUID_TEMP_3_Pin            GPIO_PIN_4
+#define OUTLET_LIQUID_TEMP_3_GPIO_Port      GPIOC
+#define OUTLET_LIQUID_TEMP_4_Pin            GPIO_PIN_5
+#define OUTLET_LIQUID_TEMP_4_GPIO_Port      GPIOC
+#define OUTLET_LIQUID_TEMP_5_Pin            GPIO_PIN_0
+#define OUTLET_LIQUID_TEMP_5_GPIO_Port      GPIOB
+#define OUTLET_LIQUID_TEMP_6_Pin            GPIO_PIN_1
+#define OUTLET_LIQUID_TEMP_6_GPIO_Port      GPIOB
+#define RADIATOR_PWM_Pin                    GPIO_PIN_2
+#define RADIATOR_PWM_GPIO_Port              GPIOB
+#define SEG7_S1_Pin                         GPIO_PIN_7
+#define SEG7_S1_GPIO_Port                   GPIOE
+#define SEG7_S2_Pin                         GPIO_PIN_8
+#define SEG7_S2_GPIO_Port                   GPIOE
+#define SEG7_S3_Pin                         GPIO_PIN_9
+#define SEG7_S3_GPIO_Port                   GPIOE
+#define SEG7_S4_Pin                         GPIO_PIN_10
+#define SEG7_S4_GPIO_Port                   GPIOE
+#define SEG7_S5_Pin                         GPIO_PIN_12
+#define SEG7_S5_GPIO_Port                   GPIOE
+#define SEG7_S6_Pin                         GPIO_PIN_13
+#define SEG7_S6_GPIO_Port                   GPIOE
+#define SEG7_S7_Pin                         GPIO_PIN_14
+#define SEG7_S7_GPIO_Port                   GPIOE
+#define SEG7_DP_Pin                         GPIO_PIN_15
+#define SEG7_DP_GPIO_Port                   GPIOE
+#define ISOCAN_RX_Pin                       GPIO_PIN_12
+#define ISOCAN_RX_GPIO_Port                 GPIOB
+#define ISOCAN_TX_Pin                       GPIO_PIN_13
+#define ISOCAN_TX_GPIO_Port                 GPIOB
+#define CS_EEPROM_Pin                       GPIO_PIN_8
+#define CS_EEPROM_GPIO_Port                 GPIOD
+#define HOLD_EEPROM_Pin                     GPIO_PIN_9
+#define HOLD_EEPROM_GPIO_Port               GPIOD
+#define IMD_PWM_Pin                         GPIO_PIN_12
+#define IMD_PWM_GPIO_Port                   GPIOD
+#define LED_2_Pin                           GPIO_PIN_14
+#define LED_2_GPIO_Port                     GPIOD
+#define LED_1_Pin                           GPIO_PIN_15
+#define LED_1_GPIO_Port                     GPIOD
+#define BMS_OK_Pin                          GPIO_PIN_5
+#define BMS_OK_GPIO_Port                    GPIOG
+#define PRECHARGE_Pin                       GPIO_PIN_6
+#define PRECHARGE_GPIO_Port                 GPIOG
+#define AIRN_OFF_Pin                        GPIO_PIN_7
+#define AIRN_OFF_GPIO_Port                  GPIOG
+#define AIRP_OFF_Pin                        GPIO_PIN_8
+#define AIRP_OFF_GPIO_Port                  GPIOG
+#define BUZZER_Pin                          GPIO_PIN_8
+#define BUZZER_GPIO_Port                    GPIOA
+#define SPI_MAX_CS_Pin                      GPIO_PIN_15
+#define SPI_MAX_CS_GPIO_Port                GPIOA
+#define SPI_MAX_SCK_Pin                     GPIO_PIN_10
+#define SPI_MAX_SCK_GPIO_Port               GPIOC
+#define SPI_MAX_MISO_Pin                    GPIO_PIN_11
+#define SPI_MAX_MISO_GPIO_Port              GPIOC
+#define SPI_MAX_MOSI_Pin                    GPIO_PIN_12
+#define SPI_MAX_MOSI_GPIO_Port              GPIOC
+#define MAX_IT_Pin                          GPIO_PIN_0
+#define MAX_IT_GPIO_Port                    GPIOD
+#define EXT_FAULT_LATCHED_Pin               GPIO_PIN_1
+#define EXT_FAULT_LATCHED_GPIO_Port         GPIOD
+#define IMD_FAULT_LATCHED_Pin               GPIO_PIN_2
+#define IMD_FAULT_LATCHED_GPIO_Port         GPIOD
+#define BMS_FAULT_LATCHED_Pin               GPIO_PIN_3
+#define BMS_FAULT_LATCHED_GPIO_Port         GPIOD
+#define IMPLAUSIBLE_STATE_LATCHED_Pin       GPIO_PIN_4
+#define IMPLAUSIBLE_STATE_LATCHED_GPIO_Port GPIOD
+#define SPARE_13_Pin                        GPIO_PIN_6
+#define SPARE_13_GPIO_Port                  GPIOD
+#define LATCH_RESET_Pin                     GPIO_PIN_7
+#define LATCH_RESET_GPIO_Port               GPIOD
+#define INDICATOR_CONNECTED_Pin             GPIO_PIN_9
+#define INDICATOR_CONNECTED_GPIO_Port       GPIOG
+#define IMD_FAULT_COCKPID_LED_Pin           GPIO_PIN_10
+#define IMD_FAULT_COCKPID_LED_GPIO_Port     GPIOG
+#define BMS_FAULT_COCKPID_LED_Pin           GPIO_PIN_11
+#define BMS_FAULT_COCKPID_LED_GPIO_Port     GPIOG
+#define PLAUSIBLE_STATE_Pin                 GPIO_PIN_12
+#define PLAUSIBLE_STATE_GPIO_Port           GPIOG
+#define PLAUSIBLE_STATE_PERSISTED_Pin       GPIO_PIN_13
+#define PLAUSIBLE_STATE_PERSISTED_GPIO_Port GPIOG
+#define TS_LESS_THAN_60V_Pin                GPIO_PIN_14
+#define TS_LESS_THAN_60V_GPIO_Port          GPIOG
+#define PRECHARGE_OPEN_MEC_Pin              GPIO_PIN_15
+#define PRECHARGE_OPEN_MEC_GPIO_Port        GPIOG
+#define PRECHARGE_OPEN_COM_Pin              GPIO_PIN_3
+#define PRECHARGE_OPEN_COM_GPIO_Port        GPIOB
+#define SPARE_12_Pin                        GPIO_PIN_4
+#define SPARE_12_GPIO_Port                  GPIOB
+#define SPARE_14_Pin                        GPIO_PIN_5
+#define SPARE_14_GPIO_Port                  GPIOB
+#define SPARE_15_Pin                        GPIO_PIN_6
+#define SPARE_15_GPIO_Port                  GPIOB
+#define SPARE_16_Pin                        GPIO_PIN_7
+#define SPARE_16_GPIO_Port                  GPIOB
+#define SD_BMS_FB_Pin                       GPIO_PIN_8
+#define SD_BMS_FB_GPIO_Port                 GPIOB
+#define SD_IMD_FB_Pin                       GPIO_PIN_9
+#define SD_IMD_FB_GPIO_Port                 GPIOB
+#define AIRP_OPEN_COM_Pin                   GPIO_PIN_0
+#define AIRP_OPEN_COM_GPIO_Port             GPIOE
+#define AIRN_OPEN_COM_Pin                   GPIO_PIN_1
+#define AIRN_OPEN_COM_GPIO_Port             GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/adc.c
@@ -0,0 +1,283 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    adc.c
+  * @brief   This file provides code for the configuration
+  *          of the ADC instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "adc.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc3;
+
+/* ADC1 init function */
+void MX_ADC1_Init(void) {
+    /* USER CODE BEGIN ADC1_Init 0 */
+
+    /* USER CODE END ADC1_Init 0 */
+
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    /* USER CODE BEGIN ADC1_Init 1 */
+
+    /* USER CODE END ADC1_Init 1 */
+
+    /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+    hadc1.Instance                   = ADC1;
+    hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV2;
+    hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
+    hadc1.Init.ScanConvMode          = DISABLE;
+    hadc1.Init.ContinuousConvMode    = DISABLE;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
+    hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion       = 1;
+    hadc1.Init.DMAContinuousRequests = DISABLE;
+    hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+    if (HAL_ADC_Init(&hadc1) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+    sConfig.Channel      = ADC_CHANNEL_0;
+    sConfig.Rank         = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN ADC1_Init 2 */
+
+    /* USER CODE END ADC1_Init 2 */
+}
+/* ADC3 init function */
+void MX_ADC3_Init(void) {
+    /* USER CODE BEGIN ADC3_Init 0 */
+
+    /* USER CODE END ADC3_Init 0 */
+
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    /* USER CODE BEGIN ADC3_Init 1 */
+
+    /* USER CODE END ADC3_Init 1 */
+
+    /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+    hadc3.Instance                   = ADC3;
+    hadc3.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV2;
+    hadc3.Init.Resolution            = ADC_RESOLUTION_12B;
+    hadc3.Init.ScanConvMode          = DISABLE;
+    hadc3.Init.ContinuousConvMode    = DISABLE;
+    hadc3.Init.DiscontinuousConvMode = DISABLE;
+    hadc3.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc3.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
+    hadc3.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+    hadc3.Init.NbrOfConversion       = 1;
+    hadc3.Init.DMAContinuousRequests = DISABLE;
+    hadc3.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+    if (HAL_ADC_Init(&hadc3) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+    sConfig.Channel      = ADC_CHANNEL_15;
+    sConfig.Rank         = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+    if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN ADC3_Init 2 */
+
+    /* USER CODE END ADC3_Init 2 */
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *adcHandle) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (adcHandle->Instance == ADC1) {
+        /* USER CODE BEGIN ADC1_MspInit 0 */
+
+        /* USER CODE END ADC1_MspInit 0 */
+        /* ADC1 clock enable */
+        __HAL_RCC_ADC1_CLK_ENABLE();
+
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**ADC1 GPIO Configuration
+    PC0     ------> ADC1_IN10
+    PC1     ------> ADC1_IN11
+    PC2     ------> ADC1_IN12
+    PC3     ------> ADC1_IN13
+    PA0-WKUP     ------> ADC1_IN0
+    PA1     ------> ADC1_IN1
+    PA2     ------> ADC1_IN2
+    PA3     ------> ADC1_IN3
+    PA5     ------> ADC1_IN5
+    PA6     ------> ADC1_IN6
+    PA7     ------> ADC1_IN7
+    PC4     ------> ADC1_IN14
+    PC5     ------> ADC1_IN15
+    PB0     ------> ADC1_IN8
+    PB1     ------> ADC1_IN9
+    */
+        GPIO_InitStruct.Pin = COOLING_CONSUMPTION_Pin | ITS_HC_F_Pin | ITS_LC_F_Pin | AUX_TEMP_Pin | OUTLET_LIQUID_TEMP_3_Pin |
+                              OUTLET_LIQUID_TEMP_4_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin = SD_OUT_Pin | SD_IN_Pin | SD_END_Pin | V5_MCU_Pin | INLET_LIQUID_TEMP_Pin | OUTLET_LIQUID_TEMP_1_Pin |
+                              OUTLET_LIQUID_TEMP_2_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin  = OUTLET_LIQUID_TEMP_5_Pin | OUTLET_LIQUID_TEMP_6_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        /* ADC1 interrupt Init */
+        HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(ADC_IRQn);
+        /* USER CODE BEGIN ADC1_MspInit 1 */
+
+        /* USER CODE END ADC1_MspInit 1 */
+    } else if (adcHandle->Instance == ADC3) {
+        /* USER CODE BEGIN ADC3_MspInit 0 */
+
+        /* USER CODE END ADC3_MspInit 0 */
+        /* ADC3 clock enable */
+        __HAL_RCC_ADC3_CLK_ENABLE();
+
+        __HAL_RCC_GPIOF_CLK_ENABLE();
+        /**ADC3 GPIO Configuration
+    PF3     ------> ADC3_IN9
+    PF4     ------> ADC3_IN14
+    PF5     ------> ADC3_IN15
+    PF6     ------> ADC3_IN4
+    PF7     ------> ADC3_IN5
+    PF9     ------> ADC3_IN7
+    PF10     ------> ADC3_IN8
+    */
+        GPIO_InitStruct.Pin = AIRN_OPEN_MEC_Pin | AIRP_OPEN_MEC_Pin | IMD_OK_Pin | PLAUSIBLE_STATE_RC_Pin | TSAL_GREEN_Pin | AMB_TEMP_Pin |
+                              PROBING_3V3_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+        /* ADC3 interrupt Init */
+        HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(ADC_IRQn);
+        /* USER CODE BEGIN ADC3_MspInit 1 */
+
+        /* USER CODE END ADC3_MspInit 1 */
+    }
+}
+
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *adcHandle) {
+    if (adcHandle->Instance == ADC1) {
+        /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+        /* USER CODE END ADC1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_ADC1_CLK_DISABLE();
+
+        /**ADC1 GPIO Configuration
+    PC0     ------> ADC1_IN10
+    PC1     ------> ADC1_IN11
+    PC2     ------> ADC1_IN12
+    PC3     ------> ADC1_IN13
+    PA0-WKUP     ------> ADC1_IN0
+    PA1     ------> ADC1_IN1
+    PA2     ------> ADC1_IN2
+    PA3     ------> ADC1_IN3
+    PA5     ------> ADC1_IN5
+    PA6     ------> ADC1_IN6
+    PA7     ------> ADC1_IN7
+    PC4     ------> ADC1_IN14
+    PC5     ------> ADC1_IN15
+    PB0     ------> ADC1_IN8
+    PB1     ------> ADC1_IN9
+    */
+        HAL_GPIO_DeInit(
+            GPIOC,
+            COOLING_CONSUMPTION_Pin | ITS_HC_F_Pin | ITS_LC_F_Pin | AUX_TEMP_Pin | OUTLET_LIQUID_TEMP_3_Pin | OUTLET_LIQUID_TEMP_4_Pin);
+
+        HAL_GPIO_DeInit(
+            GPIOA,
+            SD_OUT_Pin | SD_IN_Pin | SD_END_Pin | V5_MCU_Pin | INLET_LIQUID_TEMP_Pin | OUTLET_LIQUID_TEMP_1_Pin | OUTLET_LIQUID_TEMP_2_Pin);
+
+        HAL_GPIO_DeInit(GPIOB, OUTLET_LIQUID_TEMP_5_Pin | OUTLET_LIQUID_TEMP_6_Pin);
+
+        /* ADC1 interrupt Deinit */
+        /* USER CODE BEGIN ADC1:ADC_IRQn disable */
+        /**
+    * Uncomment the line below to disable the "ADC_IRQn" interrupt
+    * Be aware, disabling shared interrupt may affect other IPs
+    */
+        /* HAL_NVIC_DisableIRQ(ADC_IRQn); */
+        /* USER CODE END ADC1:ADC_IRQn disable */
+
+        /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+        /* USER CODE END ADC1_MspDeInit 1 */
+    } else if (adcHandle->Instance == ADC3) {
+        /* USER CODE BEGIN ADC3_MspDeInit 0 */
+
+        /* USER CODE END ADC3_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_ADC3_CLK_DISABLE();
+
+        /**ADC3 GPIO Configuration
+    PF3     ------> ADC3_IN9
+    PF4     ------> ADC3_IN14
+    PF5     ------> ADC3_IN15
+    PF6     ------> ADC3_IN4
+    PF7     ------> ADC3_IN5
+    PF9     ------> ADC3_IN7
+    PF10     ------> ADC3_IN8
+    */
+        HAL_GPIO_DeInit(
+            GPIOF,
+            AIRN_OPEN_MEC_Pin | AIRP_OPEN_MEC_Pin | IMD_OK_Pin | PLAUSIBLE_STATE_RC_Pin | TSAL_GREEN_Pin | AMB_TEMP_Pin | PROBING_3V3_Pin);
+
+        /* ADC3 interrupt Deinit */
+        /* USER CODE BEGIN ADC3:ADC_IRQn disable */
+        /**
+    * Uncomment the line below to disable the "ADC_IRQn" interrupt
+    * Be aware, disabling shared interrupt may affect other IPs
+    */
+        /* HAL_NVIC_DisableIRQ(ADC_IRQn); */
+        /* USER CODE END ADC3:ADC_IRQn disable */
+
+        /* USER CODE BEGIN ADC3_MspDeInit 1 */
+
+        /* USER CODE END ADC3_MspDeInit 1 */
+    }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
