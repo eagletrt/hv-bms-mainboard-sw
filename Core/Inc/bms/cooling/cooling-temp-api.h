c46f814e39f66c69032f4dd13a7182eb58bb70a7
Alessandro Giustina <alexjusti67n@gmail.com>
2026-05-21 09:16:39 +0200

    fix: refactor cooling temp module

---
+++ Core/Inc/bms/cooling/cooling-temp-api.h
@@ -0,0 +1,103 @@
+/*!
+ * \file cooling-temp-api.h
+ * \date 2024-09-24
+ * \author Antonio Gelain [antonio.gelain2@gmail.com]
+ * \author Alessandro Giustina [giustinalessandro@gmail.com]
+ *
+ * \brief Cooling temperature measurment and control
+ */
+
+#ifndef COOLING_TEMP_API_H
+#define COOLING_TEMP_API_H
+
+#include "cooling-temp.h"
+
+#ifdef CONF_COOLING_TEMPERATURE_MODULE_ENABLE
+
+/*!
+ * \brief Initialize the cooling temperature module
+ *
+ * \retval COOLING_TEMP_OK the init always succeeds as it just clears the handler.
+ */
+enum CoolingTempReturnCode cooling_temp_api_init(void);
+
+/*!
+ * \brief Callback used in the adc to notify the cooling temperature module that the conversion is completed
+ *
+ * \param index The idx of the value to be updated
+ * \param value The voltage to copy in V
+ *
+ * \retval COOLING_TEMP_RC_OUT_OF_BOUNDS if the index is greater than the total number of values or the voltage value is out of bounds
+ * \retval COOLING_TEMP_RC_OK if the value is updated successfully
+ */
+enum CoolingTempReturnCode cooling_temp_api_notify_conversion_complete(size_t index, volt_t value);
+
+/*!
+ * \brief Update a single temperature value
+ *
+ * \param index The index of the value to update
+ * \param value The new value
+ *
+ * \retval COOLING_TEMP_RC_OUT_OF_BOUNDS if the index is greater than the total number of values
+ * \retval COOLING_TEMP_RC_OK otherwise
+ */
+enum CoolingTempReturnCode cooling_temp_api_update_value(size_t index, celsius_t value);
+
+/*!
+ * \brief Get a pointer to the array where the temperature values are stored
+ *
+ * \returns cooling_temps* The pointer to the array
+ */
+const cooling_temps *cooling_temp_api_get_values(void);
+
+/*!
+ * \brief Get the minimum temperature in the cooling loop
+ *
+ * \returns celsius_t The minimum temperature value in °C
+ */
+celsius_t cooling_temp_api_get_min(void);
+
+/*!
+ * \brief Get the maximum temperature in the cooling loop
+ *
+ * \returns celsius_t The maximum temperature value in °C
+ */
+celsius_t cooling_temp_api_get_max(void);
+
+/*!
+ * \brief Get the sum of the temperatures of the cooling loop
+ *
+ * \returns celsius_t The sum of the temperatures in °C
+ */
+celsius_t cooling_temp_api_get_sum(void);
+
+/*!
+ * \brief Get the average temperature of the cooling loop
+ *
+ * \returns celsius_t The average temperature in °C
+ */
+celsius_t cooling_temp_api_get_avg(void);
+
+/*!
+ * \brief Get a pointer to the CAN payload of the cooling temperatures
+ *
+ * \param byte_size[out] A pointer where the size of the payload in bytes is stored (can be NULL)
+ *
+ * \returns primary_hv_cooling_temperature_converted_t* A pointer to the payload
+ */
+primary_hv_cooling_temperature_converted_t *cooling_temp_api_get_temperatures_canlib_payload(size_t *byte_size);
+
+#else
+
+#define cooling_temp_init() (COOLING_TEMP_OK)
+#define cooling_temp_notify_conversion_complete(index, value) (COOLING_TEMP_OK)
+#define cooling_temp_get_values() (NULL)
+#define cooling_temp_get_min() (NULL)
+#define cooling_temp_get_max() (NULL)
+#define cooling_temp_get_sum() (NULL)
+#define cooling_temp_get_avg() (NULL)
+#define cooling_temp_get_temperatures_canlib_payload(byte_size) (NULL)
+
+#endif // CONF_COOLING_TEMPERATURE_MODULE_ENABLE
+
+#endif // COOLING_TEMP_API_H
---
+++ Core/Src/bms/cooling/cooling-temp-api.c
@@ -0,0 +1,154 @@
+/*!
+ * \file cooling-temp-api.h
+ * \date 2024-09-24
+ * \author Antonio Gelain [antonio.gelain2@gmail.com]
+ * \author Alessandro Giustina [giustinalessandro@gmail.com]
+ *
+ * \brief Cooling temperature measurment and control
+ */
+
+#include "cooling-temp-api.h"
+
+#include <string.h>
+
+#include "error.h"
+
+#ifdef CONF_COOLING_TEMPERATURE_MODULE_ENABLE
+
+EAGLETRT_STATIC struct CoolingTempHandler cooling_temp_handler;
+
+/*!
+ * \brief Convert a voltage into a temperature using a polynomial conversion
+ *
+ * \param value The voltage value in V
+ *
+ * \return celsius_t The converted value in °C
+ */
+celsius_t prv_cooling_temp_volt_to_celsius(volt_t value) {
+    // Value is converted in V and limited to fit the polynomial range
+
+    constexpr volt_t value_min = COOLING_TEMP_MIN_LIMIT_V;
+    constexpr volt_t value_max = COOLING_TEMP_MAX_LIMIT_V;
+
+    value = MAINBOARD_CLAMP(value, value_min, value_max);
+    const double v0 = value;
+    const double v2 = v0 * v0;
+    const double v3 = v2 * v0;
+    const double v4 = v2 * v2;
+    const double v5 = v4 * v0;
+    const double v6 = v3 * v3;
+    return COOLING_TEMP_COEFF_0 +
+           (COOLING_TEMP_COEFF_1 * v0) +
+           (COOLING_TEMP_COEFF_2 * v2) +
+           (COOLING_TEMP_COEFF_3 * v3) +
+           (COOLING_TEMP_COEFF_4 * v4) +
+           (COOLING_TEMP_COEFF_5 * v5) +
+           (COOLING_TEMP_COEFF_6 * v6);
+}
+
+/*!
+ * \brief Check if the cells temperature values are in range otherwise set an error
+ *
+ * \param value The temperature value to check in °C
+ * \param index The index of the error instance to set/reset in case of out of bounds value
+ */
+EAGLETRT_STATIC_INLINE void prv_cooling_temp_check_value(const size_t index, const celsius_t value) {
+    if (value <= COOLING_TEMP_MIN_C) {
+        (void)error_set(ERROR_GROUP_COOLING_UNDER_TEMPERATURE, index);
+    } else {
+        (void)error_reset(ERROR_GROUP_COOLING_UNDER_TEMPERATURE, index);
+    }
+    if (value >= COOLING_TEMP_MAX_C) {
+        (void)error_set(ERROR_GROUP_COOLING_OVER_TEMPERATURE, index);
+    } else {
+        (void)error_reset(ERROR_GROUP_COOLING_OVER_TEMPERATURE, index);
+    }
+}
+
+enum CoolingTempReturnCode cooling_temp_api_init(void) {
+    memset(&cooling_temp_handler, 0U, sizeof(cooling_temp_handler));
+    return COOLING_TEMP_RC_OK;
+}
+
+enum CoolingTempReturnCode cooling_temp_api_notify_conversion_complete(size_t index, const volt_t value) {
+    // Convert the raw value to celsius
+    const celsius_t temp = prv_cooling_temp_volt_to_celsius(value);
+    return cooling_temp_api_update_value(index, temp);
+}
+
+enum CoolingTempReturnCode cooling_temp_api_update_value(const size_t index, const celsius_t value) {
+    if (index >= COOLING_TEMP_COUNT) {
+        return COOLING_TEMP_RC_OUT_OF_BOUNDS;
+    }
+    cooling_temp_handler.temperatures[index] = value;
+    // prv_cooling_temp_check_value(index, value);
+    return COOLING_TEMP_RC_OK;
+}
+
+const cooling_temps *cooling_temp_api_get_values(void) {
+    return &cooling_temp_handler.temperatures;
+}
+
+celsius_t cooling_temp_api_get_min(void) {
+    celsius_t min = cooling_temp_handler.temperatures[0U];
+    for (size_t i = 0U; i < COOLING_TEMP_COUNT; ++i) {
+        min = MAINBOARD_MIN(min, cooling_temp_handler.temperatures[i]);
+    }
+    return min;
+}
+
+celsius_t cooling_temp_api_get_max(void) {
+    celsius_t max = cooling_temp_handler.temperatures[0U];
+    for (size_t i = 0U; i < COOLING_TEMP_COUNT; ++i) {
+        max = MAINBOARD_MAX(max, cooling_temp_handler.temperatures[i]);
+    }
+    return max;
+}
+
+celsius_t cooling_temp_api_get_sum(void) {
+    celsius_t sum = 0U;
+    for (size_t i = 0U; i < COOLING_TEMP_COUNT; ++i) {
+        sum += cooling_temp_handler.temperatures[i];
+    }
+    return sum;
+}
+
+celsius_t cooling_temp_api_get_avg(void) {
+    return cooling_temp_api_get_sum() / COOLING_TEMP_COUNT;
+}
+
+primary_hv_cooling_temperature_converted_t *cooling_temp_api_get_temperatures_canlib_payload(size_t *const byte_size) {
+    if (byte_size != NULL) {
+        *byte_size = sizeof(cooling_temp_handler.cooling_temp_can_payload);
+    }
+
+    const celsius_t *temps = cooling_temp_handler.temperatures;
+    cooling_temp_handler.cooling_temp_can_payload.inlet = temps[COOLING_TEMP_INDEX_INLET_LIQUID_TEMPERATURE];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_0 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_1];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_1 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_2];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_2 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_3];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_3 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_4];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_4 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_5];
+    cooling_temp_handler.cooling_temp_can_payload.outlet_5 = temps[COOLING_TEMP_INDEX_OUTLET_LIQUID_TEMPERATURE_6];
+    return &cooling_temp_handler.cooling_temp_can_payload;
+}
+
+#ifdef CONF_COOLING_TEMPERATURE_STRINGS_ENABLE
+
+EAGLETRT_STATIC char *cooling_temp_module_name = "cooling temperature";
+
+EAGLETRT_STATIC char *cooling_temp_return_code_name[] = {
+    [COOLING_TEMP_OK] = "ok",
+    [COOLING_TEMP_NULL_POINTER] = "null pointer",
+    [COOLING_TEMP_OUT_OF_BOUNDS] = "out of bounds"
+};
+
+EAGLETRT_STATIC char *cooling_temp_return_code_description[] = {
+    [COOLING_TEMP_OK] = "executed successfully",
+    [COOLING_TEMP_NULL_POINTER] = "attempt to dereference a null pointer",
+    [COOLING_TEMP_OUT_OF_BOUNDS] = "attempt to access an invalid memory region"
+};
+
+#endif //  CONF_COOLING_TEMPERATURE_STRINGS_ENABLE
+
+#endif //  CONF_COOLING_TEMPERATURE_MODULE_ENABLE
