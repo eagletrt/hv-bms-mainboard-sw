9811429ccf5a4c0bef115fd7033cedcc4254cbf2
Alessandro Giustina <alexjusti67n@gmail.com>
2026-05-22 16:56:55 +0200

    fix: refactor header and implementation

---
+++ Core/Inc/bms/internal-voltage/internal-voltage.h
@@ -0,0 +1,71 @@
+/*!
+ * \file internal-voltage.h
+ * \date 2024-07-19
+ * \author Antonio Gelain [antonio.gelain2@gmail.com]
+ * \author Alessandro Giustina [giustinalessandro@gmail.com]
+ *
+ * \brief Internal voltages measurements and controls
+ */
+
+#ifndef INTERNAL_VOLTAGE_H
+#define INTERNAL_VOLTAGE_H
+
+#include "mainboard-def.h"
+#include "mainboard-conf.h"
+
+#include "primary_network.h"
+
+#include "max22530.h"
+
+/*! \brief The period with which internal voltages are updated in ms */
+#define INTERNAL_VOLTAGE_CYCLE_TIME_MS (8U)
+
+/*! \brief Maximum allowed voltage difference between the pack voltage and the cells voltage sum in V */
+#define INTERNAL_VOLTAGE_MAX_DELTA_V (25.f)
+
+/*! \brief Divider ratio of the internal voltages */
+#define INTERNAL_VOLTAGE_DIVIDER_RATIO (0.002914f)
+
+/*!
+ * \brief Conversion from the voltage read from the ADC to the real voltage in V
+ *
+ * \param value The voltage read from the ADC
+ *
+ * \returns volt_t The converted value in V
+ */
+#define INTERNAL_VOLTAGE_ADC_VOLTAGE_TO_VOLT(value) (value / INTERNAL_VOLTAGE_DIVIDER_RATIO)
+
+/*!
+ * \brief Return code for the internal voltage module functions
+ */
+enum InternalVoltageReturnCode {
+    INTERNAL_VOLTAGE_RC_OK,          /*!< Function executed successfully */
+    INTERNAL_VOLTAGE_RC_NULL_POINTER /*!< NULL pointer was given to a function */
+};
+
+/*!
+ * \brief Internal voltage channels of the external ADC
+ */
+enum InternalVoltageChannel {
+    INTERNAL_VOLTAGE_CHANNEL_TS_VOLTAGE = MAX22530_CHANNEL_1,            /*!< The voltage of the TS */
+    INTERNAL_VOLTAGE_CHANNEL_PACK_VOLTAGE = MAX22530_CHANNEL_2,          /*!< The voltage of the battery pack */
+    INTERNAL_VOLTAGE_CHANNEL_IMD_TS_CONNECTED = MAX22530_CHANNEL_3,      /*!< Feedback on the TS connection to the IMD */
+    INTERNAL_VOLTAGE_CHANNEL_PRECHARGE_TEMPERATURE = MAX22530_CHANNEL_4, /*!< Temperature of the precharge resistors heatsink */
+    INTERNAL_VOLTAGE_CHANNEL_COUNT = MAX22530_CHANNEL_COUNT              /*!< The number of channels used for internal voltage measurements */
+};
+
+/*!
+ * \brief Type definition for the internal voltage handler structure
+ *
+ * \attention This structure should not be used outside of this module
+ */
+struct InternalVoltageHandler {
+    Max22530Handler max22530; /*!< Handler structure of the external ADC */
+
+    volt_t ts;   /*!< The voltage of the Tractive System */
+    volt_t pack; /*!< The voltage of the battery pack */
+
+    primary_hv_ts_voltage_converted_t ts_voltage_can_payload; /*!< The TS voltage info canlib payload */
+};
+
+#endif // INTERNAL_VOLTAGE_H
--- Core/Src/bms/current.c
+++ Core/Src/bms/current.c
@@ -27,7 +27,6 @@ void _current_sensor_communcation_timeout(void) {
     error_set(ERROR_GROUP_CURRENT_SENSOR_COMMUNICATION, 0U);
 }
 
-
 /**
  * @brief Check if the current values are in range otherwise set an error
  *
@@ -50,8 +49,7 @@ CurrentReturnCode current_init(void) {
     (void)watchdog_init(
         &hcurrent.sensor_wdg,
         CURRENT_SENSOR_COMMUNICATION_TIMEOUT_MS,
-        _current_sensor_communcation_timeout
-    );
+        _current_sensor_communcation_timeout);
     return CURRENT_OK;
 }
 
@@ -60,14 +58,14 @@ ampere_t current_get_current(void) {
 }
 
 kilowatt_t current_get_power(void) {
-    return hcurrent.current * internal_voltage_get_ts() * 0.001;
+    return hcurrent.current * internal_voltage_api_get_ts() * 0.001;
 }
 
 WatchdogReturnCode current_start_sensor_communication_watchdog(void) {
     return watchdog_start(&hcurrent.sensor_wdg);
 }
 
-void current_handle(bms_ivt_msg_result_i_t * const payload) {
+void current_handle(bms_ivt_msg_result_i_t *const payload) {
     watchdog_reset(&hcurrent.sensor_wdg);
     if (payload == NULL)
         return;
@@ -76,14 +74,14 @@ void current_handle(bms_ivt_msg_result_i_t * const payload) {
     _current_check_value(hcurrent.current);
 }
 
-primary_hv_current_converted_t * current_get_current_canlib_payload(size_t * const byte_size) {
+primary_hv_current_converted_t *current_get_current_canlib_payload(size_t *const byte_size) {
     if (byte_size != NULL)
         *byte_size = sizeof(hcurrent.current_can_payload);
     hcurrent.current_can_payload.current = hcurrent.current;
     return &hcurrent.current_can_payload;
 }
 
-primary_hv_power_converted_t * current_get_power_canlib_payload(size_t * const byte_size) {
+primary_hv_power_converted_t *current_get_power_canlib_payload(size_t *const byte_size) {
     if (byte_size != NULL)
         *byte_size = sizeof(hcurrent.power_can_payload);
     hcurrent.power_can_payload.power = current_get_power();
@@ -92,13 +90,13 @@ primary_hv_power_converted_t * current_get_power_canlib_payload(size_t * const b
 
 #ifdef CONF_CURRENT_STRINGS_ENABLE
 
-_STATIC char * current_module_name = "current";
+_STATIC char *current_module_name = "current";
 
-_STATIC char * current_return_code_name[] = {
+_STATIC char *current_return_code_name[] = {
     [CURRENT_OK] = "ok"
 };
 
-_STATIC char * current_return_code_description[] = {
+_STATIC char *current_return_code_description[] = {
     [CURRENT_OK] = "executed succesfully"
 };
 
