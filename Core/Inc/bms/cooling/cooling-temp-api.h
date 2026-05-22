ad6def5f37c6f6e0725addfa8cfba2d3562bbddf
Alessandro Giustina <alexjusti67n@gmail.com>
2026-05-22 15:51:27 +0200

    fix: change function names

--- Core/Inc/bms/cooling/cooling-temp-api.h
+++ Core/Inc/bms/cooling/cooling-temp-api.h
@@ -89,14 +89,14 @@ primary_hv_cooling_temperature_converted_t *cooling_temp_api_get_temperatures_ca
 
 #else
 
-#define cooling_temp_init() (COOLING_TEMP_OK)
-#define cooling_temp_notify_conversion_complete(index, value) (COOLING_TEMP_OK)
-#define cooling_temp_get_values() (NULL)
-#define cooling_temp_get_min() (NULL)
-#define cooling_temp_get_max() (NULL)
-#define cooling_temp_get_sum() (NULL)
-#define cooling_temp_get_avg() (NULL)
-#define cooling_temp_get_temperatures_canlib_payload(byte_size) (NULL)
+#define cooling_temp_api_init() (COOLING_TEMP_OK)
+#define cooling_temp_api_notify_conversion_complete(index, value) (COOLING_TEMP_OK)
+#define cooling_temp_api_get_values() (NULL)
+#define cooling_temp_api_get_min() (NULL)
+#define cooling_temp_api_get_max() (NULL)
+#define cooling_temp_api_get_sum() (NULL)
+#define cooling_temp_api_get_avg() (NULL)
+#define cooling_temp_api_get_temperatures_canlib_payload(byte_size) (NULL)
 
 #endif // CONF_COOLING_TEMPERATURE_MODULE_ENABLE
 
