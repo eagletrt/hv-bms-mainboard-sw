5d777007227b4762302ae663665f846f99aed976
Alessandro Giustina <alexjusti67n@gmail.com>
2026-06-15 11:30:19 +0200

    feat: add tests

--- Core/Src/bms/imd-api.c
+++ Core/Src/bms/imd-api.c
@@ -2,6 +2,7 @@
  * \file imd.c
  * \date 2024-07-04
  * \author Antonio Gelain [antonio.gelain2@gmail.com]
+ * \author Alessandro Giustina [giustinalessandro@gmail.com]
  *
  * \brief Insulation Monitoring Device (IMD) handing functions
  */
@@ -10,58 +11,76 @@
 
 #include "imd-api.h"
 #include "feedback.h"
+#include "eagletrt-api.h"
 
 #ifdef CONF_IMD_MODULE_ENABLE
 
-_STATIC struct ImdHandler himd;
+EAGLETRT_STATIC struct ImdHandler imd_handler;
 
 enum ImdReturnCode imd_init(const imd_pwm_start_callback start) {
-    memset(&himd, 0U, sizeof(himd));
-    himd.start = start;
-    const enum ImdReturnCode code = (const enum ImdReturnCode)ir1553204_init(&himd.ir1153204);
+    if (start == NULL) {
+        return IMD_RC_NULL_POINTER;
+    }
+    memset(&imd_handler, 0U, sizeof(imd_handler));
+    imd_handler.start = start;
+    const enum ImdReturnCode code = (const enum ImdReturnCode)ir1553204_init(&imd_handler.ir1153204);
     start();
     return code;
 }
 
 precise_percentage_t imd_get_duty_cycle(void) {
-    return ir1553204_get_duty_cycle(&himd.ir1153204);
+    return ir1553204_get_duty_cycle(&imd_handler.ir1153204);
 }
 
 hertz_t imd_get_frequency() {
-    return ir1553204_get_frequency(&himd.ir1153204);
+    return ir1553204_get_frequency(&imd_handler.ir1153204);
 }
 
 milliseconds_t imd_get_period() {
-    return ir1553204_get_period(&himd.ir1153204);
+    return ir1553204_get_period(&imd_handler.ir1153204);
 }
 
 enum ImdStatus imd_get_status() {
-    return (enum ImdStatus)ir1553204_get_status(&himd.ir1153204);
+    return (enum ImdStatus)ir1553204_get_status(&imd_handler.ir1153204);
 }
 
 enum ImdReturnCode imd_update(const ticks_t source_frequency, const ticks_t period_count, const ticks_t high_count) {
     if (period_count == 0) {
         return IMD_RC_INVALID_DATA;
     }
-    hertz_t frequency = source_frequency / (float)period_count;
-    precise_percentage_t duty_cycle = high_count / (float)period_count;
-    (void)ir1553204_set_frequency(&himd.ir1153204, frequency);
-    (void)ir1553204_set_duty_cycle(&himd.ir1153204, duty_cycle);
+    /*
+     * Signal:
+     *              ┌──────┐                   ┌──────┐
+     *              │      │                   │      │
+     * ─────────────┘      └───────────────────┘      └─────
+     *              ↑      ↑                   ↑
+     *              │      │                   │
+     *              │      └─ CCR2 = CNT       └─ CCR1 = CNT (period)
+     *              |         (high_count)          IRQ → callback fires
+     *              │                               
+     *              │
+     *              └─ CNT = 0
+     */
+
+    hertz_t frequency = (float)source_frequency / (float)period_count;
+    precise_percentage_t duty_cycle = (float)high_count / (float)period_count;
+    (void)ir1553204_set_frequency(&imd_handler.ir1153204, frequency);
+    (void)ir1553204_set_duty_cycle(&imd_handler.ir1153204, duty_cycle);
     return IMD_RC_OK;
 }
 
 primary_hv_imd_status_converted_t *imd_get_status_canlib_payload(size_t *const byte_size) {
     if (byte_size != NULL) {
-        *byte_size = sizeof(himd.status_can_payload);
+        *byte_size = sizeof(imd_handler.status_can_payload);
     }
     // The value of the IMD status of the canlib differs by one from the IMD status
     // of the BMS
-    himd.status_can_payload.status = (primary_hv_imd_status_status)(imd_get_status() + 1U);
-    himd.status_can_payload.frequency = imd_get_frequency();
-    himd.status_can_payload.duty_cycle = imd_get_duty_cycle();
-    himd.status_can_payload.feedback_not_imd_fault_cockpit_led = (primary_hv_imd_status_feedback_not_imd_fault_cockpit_led)feedback_get_status(FEEDBACK_ID_IMD_FAULT_COCKPIT_LED);
-    himd.status_can_payload.feedback_not_imd_fault_latched = (primary_hv_imd_status_feedback_not_imd_fault_latched)feedback_get_status(FEEDBACK_ID_IMD_FAULT_LATCHED);
-    return &himd.status_can_payload;
+    imd_handler.status_can_payload.status = (primary_hv_imd_status_status)(imd_get_status() + 1U);
+    imd_handler.status_can_payload.frequency = imd_get_frequency();
+    imd_handler.status_can_payload.duty_cycle = imd_get_duty_cycle();
+    imd_handler.status_can_payload.feedback_not_imd_fault_cockpit_led = (primary_hv_imd_status_feedback_not_imd_fault_cockpit_led)feedback_get_status(FEEDBACK_ID_IMD_FAULT_COCKPIT_LED);
+    imd_handler.status_can_payload.feedback_not_imd_fault_latched = (primary_hv_imd_status_feedback_not_imd_fault_latched)feedback_get_status(FEEDBACK_ID_IMD_FAULT_LATCHED);
+    return &imd_handler.status_can_payload;
 }
 
 #ifdef CONF_IMD_STRINGS_ENABLE
@@ -82,7 +101,7 @@ EAGLETRT_STATIC char *imd_return_code_description[] = {
 
 // IMD status unknown is negative so it can't be used in an array
 EAGLETRT_STATIC char *imd_status_unknown_name = "unknown";
-_EAGLETRT_STATIC char *imd_status_name[] = {
+EAGLETRT_STATIC char *imd_status_name[] = {
     [IMD_STATUS_SHORT_CIRCUIT] = "short circuit",
     [IMD_STATUS_NORMAL] = "normal",
     [IMD_STATUS_UNDER_VOLTAGE] = "undervoltage",
